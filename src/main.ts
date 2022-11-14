44e2a08efc637cace31f0d6d462c1401d64f5665
rahul.rathod <rahul.rathod@green-apex.com>
2022-11-14 18:05:17 +0530

    cm

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+});
   app.enableCors({ 
     origin: ['https://todo-omega-sepia.vercel.app', 'https://www.google.com'],
     credentials:true
