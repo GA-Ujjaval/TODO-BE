870864e79026b16ce91522ac7f5b65716beac9bd
rahul.rathod <rahul.rathod@green-apex.com>
2022-11-14 17:06:05 +0530

    added cors pac

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cors from 'cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
-    cors:true,
-  });
-  const options = {
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true
-  };
-  // app.enableCors(options);
-  app.enableCors({
-    allowedHeaders: ['content-type'],
-    origin: 'https://todo-omega-sepia.vercel.app',
-    credentials: true,
-  });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.use(cors());
   await app.listen(3000);
 }
 bootstrap();
